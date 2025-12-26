a898cb082e96d5768b20f1a176dfa8a299cd8054
Saul <luishsaul@icloud.com>
2025-12-26 03:57:08 -0300

    icones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,24 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Almoço dos Missionários",
   description: "Agendamento de almoço para missionários",
+  icons: {
+    icon: "/favicon.png",
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        rel: "icon",
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        rel: "icon",
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
