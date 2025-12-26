2af88bb0b39de4bb8ffde06d301dbc36009c68ef
Saul <luishsaul@icloud.com>
2025-12-26 04:01:31 -0300

    OGP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,22 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Almoço dos Missionários",
   description: "Agendamento de almoço para missionários",
+
+  authors: [
+    {
+      name: "Luis Henrique Engel Saul"
+    }
+  ],
+
+  keywords: [
+    "Almoço",
+    "Missionários",
+    "Agendamento",
+    "Igreja",
+    "Eventos",
+    "Sistema de agendamento"
+  ],
+
   icons: {
     icon: "/favicon.png",
     apple: "/apple-touch-icon.png",
@@ -19,6 +35,23 @@ export const metadata: Metadata = {
         url: "/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png"
+      }
+    ]
+  },
+
+  openGraph: {
+    title: "Almoço dos Missionários",
+    description: "Sistema de agendamento de almoço para missionários.",
+    url: "https://lunchsud.vercel.app",
+    siteName: "Almoço dos Missionários",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Almoço dos Missionários"
       }
     ]
   }
