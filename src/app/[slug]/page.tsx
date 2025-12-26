b2a93676f11cfc9b6f11441ee790a8a1df3a4e89
Saul <luishsaul@icloud.com>
2025-12-26 02:25:45 -0300

    1.022

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default async function Page({ params }: Props) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/agendamentos/${params.slug}`,
+    `/api/agendamentos/${params.slug}`,
     { cache: 'no-store' }
   )
 
